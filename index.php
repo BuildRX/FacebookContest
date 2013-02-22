e258cf7bc0c26035f47464813b3dec9e2bc9fd34
Matt Dunbar <matt@buildrx.com>
2013-02-21 22:30:51 -0500

    Fix to make the secret key pull from the constant, and moved the functions to the bottom of the file.

--- index.php
+++ index.php
@@ -1,26 +1,7 @@
 <?php
 require('config.php');
-$data = parse_signed_request($_REQUEST['signed_request'], $facebook_secret);
+$data = parse_signed_request($_REQUEST['signed_request'], FACEBOOK_SECRET_KEY);
 $page_data=$data['page'];
-
-function parse_signed_request($signed_request, $secret) {
-	list($encoded_sig, $payload) = explode('.', $signed_request, 2);
-	$sig = base64_url_decode($encoded_sig);
-	$data = json_decode(base64_url_decode($payload), true);
-	if (strtoupper($data['algorithm']) !== 'HMAC-SHA256') {
-		error_log('Unknown algorithm. Expected HMAC-SHA256');
-		return null;
-	}
-	$expected_sig = hash_hmac('sha256', $payload, $secret, $raw = true);
-	if ($sig !== $expected_sig) {
-		error_log('Bad Signed JSON signature!');
-		return null;
-	}
-	return $data;
-}
-function base64_url_decode($input) {
-	return base64_decode(strtr($input, '-_', '+/'));
-}
 $page_contents = '';
 if($page_data['liked'] == "1"){
 	$page_contents = file_get_contents('skin/'.POSTLIKE_BLOCK);
@@ -53,3 +34,23 @@ if($page_data['liked'] == "1"){
 		</script>
 	</body>
 </html>
+<?php
+function parse_signed_request($signed_request, $secret) {
+	list($encoded_sig, $payload) = explode('.', $signed_request, 2);
+	$sig = base64_url_decode($encoded_sig);
+	$data = json_decode(base64_url_decode($payload), true);
+	if (strtoupper($data['algorithm']) !== 'HMAC-SHA256') {
+		error_log('Unknown algorithm. Expected HMAC-SHA256');
+		return null;
+	}
+	$expected_sig = hash_hmac('sha256', $payload, $secret, $raw = true);
+	if ($sig !== $expected_sig) {
+		error_log('Bad Signed JSON signature!');
+		return null;
+	}
+	return $data;
+}
+function base64_url_decode($input) {
+	return base64_decode(strtr($input, '-_', '+/'));
+}
+?>
